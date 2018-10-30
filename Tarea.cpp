906b1a821ef15c3d76b1e9440b3a699e8e513483
tfuentes <tomas.fuentesa@sansano.usm.cl>
2018-10-30 20:12:12 -0300

    Tarea Algoritmos II

--- Tarea.cpp
+++ Tarea.cpp
@@ -5,6 +5,15 @@
 
 using namespace std;
 
+void recoded(vector<char> str){//Recorre el vector string
+  unsigned int x;
+  str.push_back(str[0]);//mueve la primera letra al final
+  str.erase(str.begin());//elimina la primera letra actual
+  for(x = 0; x < str.size(); x++){
+    cout << str[x] << '\n';
+  }
+}
+
 double factorial(double n){
   double fact;
   if (n==1 || n==0){
@@ -33,10 +42,13 @@ int main(int argc, char const *argv[]) {
   for(x = 0; x < 2*p; x++){//recorre todas las lineas
     cin >> linea;
     if (x % 2 == 0){ //letras
+      vector<char> str;
       cout << "letra 0: " << linea[0]<< "\n";
       cout << "largo palabra: " << linea.length() << '\n';
       perm = factorial(linea.length());
       cout << "total permutacion: " << perm << "\n";
+      copy(linea.begin(), linea.end(), back_inserter(str));//Funcion que convierte el string en un vector, desde el inicio (begin), hasta el final (end)
+      recoded(str);
     }
     if (x % 2 != 0){ //permutacion que busco
       busq = stoi(linea);
